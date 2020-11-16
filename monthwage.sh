d9de9b470e10fdef8d4c88620046215f8d00b5cd
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-16 22:56:14 +0530

    Added: Employee monthly Wages

---
+++ monthwage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+IS_PART_TIME=1
+IS_FULL_TIME=2
+EMP_RATE_PER_HR=20
+NUM_WORKING_DAYS=20
+totalsalary=0
+for((day=1; day<=$NUM_WORKING_DAYS; day++ ))
+do
+     random=$((RANDOM%3))
+     case $random in
+      $IS_PART_TIME)empHrs=4
+          ;;
+      $IS_FULL_TIME) empHrs=8
+          ;;
+      *)empHrs=0
+esac
+salary=$(( $empHrs * EMP_RATE_PER_HR ))
+echo "Day:$day     Salary:$salary"
+totalsalary=$(($totalsalary+$salary))
+done
+echo "Total Salary: $totalsalary"
