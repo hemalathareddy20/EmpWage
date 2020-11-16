e1721f372b4efc8aab403479365b17b1ca057776
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-16 23:43:51 +0530

    refactor: function to get work hours

---
+++ function.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+IS_PART_TIME=1
+IS_FULL_TIME=2
+EMP_RATE_PER_HR=20
+MAX_HRS_IN_MONTH=100
+NUM_WORKING_DAYS=20
+totalEmpHrs=0
+totalWorkingDays=0
+totalWorkHours=0
+declare -A dayWiseWages
+function calculateDailyWage()
+{
+empHrs=$1
+daySalary=$(( $empHrs * $EMP_RATE_PER_HR ))
+echo $daySalary
+}
+
+function getWorkingHours()
+{
+case $1 in
+$IS_FULL_TIME ) empHrs=8
+;;
+$IS_PART_TIME ) empHrs=4
+;;
+*) empHrs=0
+;;
+esac
+echo $empHrs
+}
+while(( $totalWorkHours < $MAX_HRS_IN_MONTH && $totalWorkingDays < $NUM_WORKING_DAYS ))
+do
+((totalWorkingDays++))
+#Calling function getWorkingHours by passing random number
+workHours="$( getWorkingHours $(( RANDOM%3 )) )"
+
+echo "Work hours from function getWorkingHours()::::::::::>>> $workHours "
+totalWorkHours=$(( $totalWorkHours + $workHours ))
+echo -e "TotalWorkHours: $totalWorkHours\n"
+done
+totalSalary=$(( $totalWorkHours * $EMP_RATE_PER_HR ));
+echo "Total salary: $totalSalary"
